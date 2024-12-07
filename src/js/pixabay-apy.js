5fe16c48b15dbe9303f0d33258cd482416f26f9f
Dmytro Chaika <djseagull90@gmail.com>
2024-12-08 00:54:27 +0100

    added batton, created fn

--- src/js/pixabay-apy.js
+++ src/js/pixabay-apy.js
@@ -1,23 +1,21 @@
 
 export default getPictures;
+import axios from 'axios';  
 
 
-function getPictures (tag) {
+async function  getPictures (tag, page = 1) {
     //https://pixabay.com/api/?q=&image_type=photo&orientation=horizontal&safesearch=true&key=47380819-2a2ad5e165b633e18b6fd0fd3
     const API_URL = `https://pixabay.com/api`
-    const API_KEY = `47380819-2a2ad5e165b633e18b6fd0fd3`
-    return fetch(`${API_URL}/?q=${tag}&image_type=photo&orientation=horizontal&safesearch=true&key=${API_KEY}`)
-        .then((response) => {
-       
-        if (!response.ok) {
-          throw new Error(response.statusText)
-            }
-            return response.json()
-        })
-        
+  const API_KEY = `47380819-2a2ad5e165b633e18b6fd0fd3`
+  return await axios.get(`${API_URL}/?q=${tag}&image_type=photo&orientation=horizontal&safesearch=true&key=${API_KEY}`, {
+    params: {
 
-        
-
+      page,
+      per_page: 15,
+    }
+   });
+  
+      
 }
 
 
--- src/main.js
+++ src/main.js
@@ -14,54 +14,101 @@ const litebox = new SimpleLightbox('.list-js a', {
 const form = document.querySelector(".form-js");
 const list = document.querySelector(".list-js");
 const loader = document.querySelector(".loader");
+const loadBtn = document.querySelector(".js-load-btn")
 
 
 form.addEventListener("submit", toSabmit);
+loadBtn.addEventListener("click", onLoadMore)
+let page = 1;
+
 
 function toSabmit(evt) {
     evt.preventDefault();
+    
     const { picture } = evt.target.elements;
-    if (picture.value) {
-        console.dir(picture.value)
-      
+    const value = picture.value.trim();
+    
+     list.innerHTML = ""; 
+    if(!value || value === " "){
+        { iziToast.show({
+      title:":(",         
+      message: "Please add request!",
+      position: "center",
+      color: "red"
+            });
+            list.innerHTML = ":(";
+            return
+              
+            }
     }
-    list.innerHTML = ""; 
-loader.classList.remove("hidden");
-    getPictures(picture.value)
-            .then((data) => {
-       
-            if (!data.hits.length) { iziToast.show({
+  
+    
+    loader.classList.remove("hidden");
+   loadBtn.classList.replace("more-btn", "hidden");
+    getPictures(value,page)
+    
+        .then(({ data: { hits } }) => {
+            page = 1
+      
+           console.log(page)
+            
+            if (!hits.length) { iziToast.show({
       title:"X",         
       message: "Sorry, there are no images matching your search query. Please try again!",
       position: "center",
       color: "red"
             });
                 
-               list.innerHTML = "<h1>Ooops... 👻</h1>";  
+                list.innerHTML = "<h1>Ooops... 👻</h1>";
+                loadBtn.classList.replace("more-btn", "hidden");
+                
             }
             else {
-          
-
-                list.innerHTML = createMurkup(data.hits);
+                list.innerHTML = createMurkup(hits);
                 litebox.refresh(); 
+              
+                loadBtn.classList.replace("hidden", "more-btn");
+               
+                 
                 }           
             
          })
-        .catch((error) => console.log(error.message))
+        .catch((error) => {
+            console.log(error.message)
+            iziToast.show({
+                title: "X",
+                message: `${error.message}`,
+                position: "center",
+                color: "red"
+            })
+        })
         .finally(() => {
             picture.value = "" 
             loader.classList.add("hidden");
             
+            
         })
 }
-
-
-
-
-
-
-
-
-
+async function onLoadMore() {
+    page += 1;
+    console.log(page)
+    try { 
+        const { data: { hits, totalHits
+        } } = await getPictures(page);
+        console.log(hits, totalHits); 
+        list.insertAdjacentHTML("beforeend", createMurkup(hits));
+         let totalPage = Math.round(totalHits / hits.length);
+        if (page >= totalPage || !totalHits) {
+            loadBtn.classList.replace("more-btn", "hidden");
+              iziToast.show({
+      title:"X",         
+      message: "We're sorry, but you've reached the end of search results.",
+      position: "center",
+      color: "red"
+            });  
+        }
+    }
+    catch(error){console.log(error.message)}
+}
 
 
